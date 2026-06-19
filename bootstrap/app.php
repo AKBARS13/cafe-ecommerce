eb7aaa7186394db493bbdc12ba9448e8b4c064d5
AKBARS13 <abubakarsidik1310@gmail.com>
2026-06-20 01:13:28 +0700

    fix https trust proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
+
+        // Trust all proxies (Railway uses reverse proxy)
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
